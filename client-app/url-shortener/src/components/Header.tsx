686d82c6743ff7aa427e417f2933dc746c0866d7
Ananya Dua <ananyadua@berkeley.edu>
2025-07-21 02:50:11 -0700

    stylistic

--- client-app/url-shortener/src/components/Header.tsx
+++ client-app/url-shortener/src/components/Header.tsx
@@ -3,7 +3,7 @@ import './Header.css';
 const Header = () => {
   return (
     <header className="header">
-      <h1 className="header-title">url shortener</h1>
+      <h1 className="header-title">shrinkURL</h1>
     </header>
   );
 };
